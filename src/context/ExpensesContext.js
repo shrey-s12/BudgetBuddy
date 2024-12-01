115c8ca4deed3b2c66e7e05d47b2ab1badb1fcab
Shrey Singhal <shreynbd12@gmail.com>
2024-12-01 14:22:24 +0530

    Return in useEffect

--- src/context/ExpensesContext.js
+++ src/context/ExpensesContext.js
@@ -8,15 +8,23 @@ export const ExpensesProvider = ({ children }) => {
     const [editIndex, setEditIndex] = useState(-1);
     const [expenses, setExpenses] = useState([]);
     useEffect(() => {
+        // Start of the effect
         getExpensesFromBackend()
             .then(expensesVal => setExpenses(expensesVal))
             .catch(err => console.log(err));
+        // End of the effect (Cleanup function)
+        return () => {
+            console.log("Effect Ended");
+        }
     }, []);
 
     useEffect(() => {
         setExpensesInBackend(expenses)
             .then(() => console.log("Expenses updated in local storage"))
             .catch(err => console.log(err));
+        return () => {
+            console.log("Effect Ended");
+        }
     }, [expenses])
 
     const contextObject = {
